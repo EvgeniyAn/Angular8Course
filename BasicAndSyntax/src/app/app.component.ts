a05680b462ec22784ce4993238400223b2a3a540
E.Andrienko <y.andriienko@upswot.com>
2019-11-18 12:01:07 +0200

    #14 Two way binding

--- BasicAndSyntax/src/app/app.component.ts
+++ BasicAndSyntax/src/app/app.component.ts
@@ -6,20 +6,12 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AppComponent title';
-  number = 41;
-  arr = [1, 2, 3];
-  obj = {a1: 1, b1: 2, c1: {a2: 2}};
-  inputValue: any = '';
+  title: any = 'Initial';
 
   constructor() {
   }
 
-  onInputChange(event: KeyboardEvent) {
-    this.inputValue = (event.target as HTMLInputElement).value;
-  }
-
-  onBlur(value: string) {
-    this.inputValue = value;
+  onInput(event: any) {
+    this.title = event.target.value;
   }
 }
