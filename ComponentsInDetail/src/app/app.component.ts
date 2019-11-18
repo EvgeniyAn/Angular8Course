06612496081083a029baf3732cb2a51ad02b48b6
E.Andrienko <y.andriienko@upswot.com>
2019-11-18 14:00:52 +0200

    #22 Parameter transfer

--- ComponentsInDetail/src/app/app.component.ts
+++ ComponentsInDetail/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Post {
+  title: string,
+  text: string,
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -6,4 +12,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  posts: Post[] = [
+    {id: 1, title: 'Post 1', text: 'Some text 1'},
+    {id: 2, title: 'Post 2', text: 'Some text 2'},
+    {id: 3, title: 'Post 3', text: 'Some text 3'}
+  ]
 }
