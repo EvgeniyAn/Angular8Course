c79fcdc41b27229d27178da64437957850532403
E.Andrienko <y.andriienko@upswot.com>
2019-11-18 17:57:42 +0200

    #33 Passing parameters to the directive

--- Directives/src/app/directives/style.directive.ts
+++ Directives/src/app/directives/style.directive.ts
@@ -1,21 +1,31 @@
-import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+
+  @Input('appStyle') color: string = 'blue';
+  @Input() dStyles: { border?: string, fontWeight?: string, borderRadius?: string };
+
   constructor(private elRef: ElementRef, private r: Renderer2) {
   }
 
-  @HostListener('click', ['$event.target']) onClick(event: Event){
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
     console.log(event);
   }
 
-  @HostListener('mouseenter', ['$event.target']) onEnter(){
-    this.r.setStyle(this.elRef.nativeElement, 'color', 'blue');
+  @HostListener('mouseenter', ['$event.target']) onEnter() {
+    this.r.setStyle(this.elRef.nativeElement, 'color', this.color);
+    this.r.setStyle(this.elRef.nativeElement, 'font-weight', this.dStyles.fontWeight);
+    this.r.setStyle(this.elRef.nativeElement, 'border', this.dStyles.border);
+    this.r.setStyle(this.elRef.nativeElement, 'border-radius', this.dStyles.borderRadius);
   }
 
-  @HostListener('mouseleave', ['$event.target']) onLeave(){
+  @HostListener('mouseleave', ['$event.target']) onLeave() {
     this.r.setStyle(this.elRef.nativeElement, 'color', null);
+    this.r.setStyle(this.elRef.nativeElement, 'font-weight', null);
+    this.r.setStyle(this.elRef.nativeElement, 'border', null);
+    this.r.setStyle(this.elRef.nativeElement, 'border-radius', null);
   }
 }
