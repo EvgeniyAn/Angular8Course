eec4695728ac852d98b4065fa805c717836dbe4f
E.Andrienko <y.andriienko@upswot.com>
2019-11-18 11:22:18 +0200

    #13 One way and Event Bindings

--- BasicAndSyntax/src/app/app.component.ts
+++ BasicAndSyntax/src/app/app.component.ts
@@ -6,12 +6,20 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Basic1';
+  title = 'AppComponent title';
   number = 41;
   arr = [1, 2, 3];
   obj = {a1: 1, b1: 2, c1: {a2: 2}};
+  inputValue: any = '';
 
   constructor() {
+  }
 
+  onInputChange(event: KeyboardEvent) {
+    this.inputValue = (event.target as HTMLInputElement).value;
+  }
+
+  onBlur(value: string) {
+    this.inputValue = value;
   }
 }
