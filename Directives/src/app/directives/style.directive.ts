30dfcbaed573697a791524c95306e58c02640246
E.Andrienko <y.andriienko@upswot.com>
2019-11-18 17:38:45 +0200

    #31 Access to element from directive

--- Directives/src/app/directives/style.directive.ts
+++ Directives/src/app/directives/style.directive.ts
@@ -1,8 +1,10 @@
-import {Directive} from "@angular/core";
+import {Directive, ElementRef, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
-
+  constructor(private elRef: ElementRef, private r: Renderer2) {
+    this.r.setStyle(this.elRef.nativeElement, 'color', 'blue');
+  }
 }
