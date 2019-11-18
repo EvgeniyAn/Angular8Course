83c0e3f8c20d424cb75673e5b09812128045040d
E.Andrienko <y.andriienko@upswot.com>
2019-11-18 15:27:26 +0200

    #28 Change detection strategy

--- ComponentsInDetail/src/app/app.component.ts
+++ ComponentsInDetail/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 export interface Post {
   title: string,
@@ -11,12 +11,25 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   posts: Post[] = [
-    {id: 1, title: 'Post 1', text: 'Some text 1'},
-    {id: 2, title: 'Post 2', text: 'Some text 2'},
-    {id: 3, title: 'Post 3', text: 'Some text 3'}
+    {id: 1, title: 'Post 1', text: 'Some text 1'}
+    // {id: 2, title: 'Post 2', text: 'Some text 2'},
+    // {id: 3, title: 'Post 3', text: 'Some text 3'}
   ];
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('Timeout')
+      //this.posts[0].title = 'Changed!'; //не работает если ChangeDetectionStrategy.OnPush
+      this.posts[0] = {
+        title: 'changed',
+        text: 'chnaged 2',
+        id: 33
+      };
+
+    }, 5000);
+  }
 
   updatePosts(post: Post) {
     this.posts.unshift(post);
@@ -26,5 +39,6 @@ export class AppComponent {
     console.log('Id to remove: ', id);
     this.posts = this.posts.filter(item => item.id !== id);
   }
+
 
 }
