0c6d18f0cb5581f6b75ccefc8ca5c2275e484821
E.Andrienko <y.andriienko@upswot.com>
2019-11-21 19:29:45 +0200

    #45 Pipe async

--- Pipes/src/app/app.component.ts
+++ Pipes/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
 
 export interface Post {
   title: string,
@@ -11,7 +12,7 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // e: number = Math.E;
   // str = 'hello world';
   // date: Date = new Date();
@@ -26,18 +27,39 @@ export class AppComponent {
   //     }
   //   }
   // }
-  posts: Post[] = [
-    {title: 'Beer', text: 'The best beer'},
-    {title: 'Bread', text: 'The best bread'},
-    {title: 'JavaScript', text: 'Language'}
-  ];
-  search: string;
-  searchField = 'title';
+  // posts: Post[] = [
+  //   {title: 'Beer', text: 'The best beer'},
+  //   {title: 'Bread', text: 'The best bread'},
+  //   {title: 'JavaScript', text: 'Language'}
+  // ];
+  // search: string;
+  // searchField = 'title';
+  //
+  // addPost() {
+  //   this.posts.unshift({
+  //     title:'Angular 8',
+  //     text:'Angular 8 course'
+  //   })
+  // }
 
-  addPost() {
-    this.posts.unshift({
-      title:'Angular 8',
-      text:'Angular 8 course'
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise Resolve')
+    }, 4000);
+  });
+
+  date$: Observable<Date> = new Observable<Date>(obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  });
+
+  date: Date;
+
+  ngOnInit(): void {
+    this.date$.subscribe(date => {
+      this.date = date;
     })
   }
+
 }
