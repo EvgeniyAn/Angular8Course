15fd2c7f182d49a5c2d718b73f9c6966cf0d2d3e
E.Andrienko <y.andriienko@upswot.com>
2019-11-21 18:54:02 +0200

    #41-42 Create pipes

--- Pipes/src/app/app.module.ts
+++ Pipes/src/app/app.module.ts
@@ -3,11 +3,15 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
+import {MultiByPipe} from "./pipes/mult-by.pipe";
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultiByPipe,
+    ExMarksPipe
   ],
   imports: [
     BrowserModule,
