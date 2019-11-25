d0f85684ef12935a818a8000d0535fd29509f994
E.Andrienko <y.andriienko@upswot.com>
2019-11-25 16:57:21 +0200

    #64-65 Receiving data and output to the template

--- WorkingWithServer/src/app/app.component.ts
+++ WorkingWithServer/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import {Component, OnInit} from '@angular/core'
+import {HttpClient} from "@angular/common/http";
+
+export interface Todo {
+  completed: boolean,
+  title: string,
+  id?: number
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,9 +14,17 @@ import {Component, OnInit} from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
-
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/posts?_limit=2')
+      .subscribe(todos => {
+        console.log(todos);
+        this.todos = todos
+      });
   }
 }
 
