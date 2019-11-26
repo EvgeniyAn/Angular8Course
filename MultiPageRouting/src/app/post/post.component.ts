194b05911cb7fe5922d75ec04205a04ee3ae05c4
E.Andrienko <y.andriienko@upswot.com>
2019-11-26 14:30:28 +0200

    #83 Processing parameters

--- MultiPageRouting/src/app/post/post.component.ts
+++ MultiPageRouting/src/app/post/post.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Post, PostsService} from "../posts.service";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  private post: Post;
 
+  constructor(
+    private postsService: PostsService,
+    private router: Router,
+    private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log('Params ', params);
+      this.post = this.postsService.getById(+params.id);
+    })
+  }
+
+  loadPost() {
+    this.router.navigate(['/posts', 44]);
+  }
 }
