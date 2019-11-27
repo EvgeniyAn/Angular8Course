431614dfd920b7c2b2f6245522187555b95db70e
E.Andrienko <y.andriienko@upswot.com>
2019-11-27 15:03:25 +0200

    #95 Changing the module loading strategy

--- Modules/src/app/app-routing.module.ts
+++ Modules/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import {NgModule} from '@angular/core'
-import {RouterModule} from '@angular/router'
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
 import {HomePageComponent} from './home-page/home-page.component'
 
+const routes: Routes = [
+  {path: '', component: HomePageComponent, pathMatch: 'full'},
+  // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'} // ленивая загрузка старый вариант
+  {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule)} // новый вариант
+];
+
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: '', component: HomePageComponent, pathMatch: 'full'},
-    // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'} // ленивая загрузка старый вариант
-    {path: 'about', loadChildren: ()=> import('./about-page/about-page.module').then(m=>m.AboutPageModule)} // новый вариант
-  ])],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
