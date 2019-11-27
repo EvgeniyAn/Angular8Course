40d1db8d9b0968d76728f8c2f26cd9b63a13a115
E.Andrienko <y.andriienko@upswot.com>
2019-11-27 15:00:17 +0200

    #94 Lazy module loading. New variant

--- Modules/src/app/app-routing.module.ts
+++ Modules/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import {HomePageComponent} from './home-page/home-page.component'
 @NgModule({
   imports: [RouterModule.forRoot([
     {path: '', component: HomePageComponent, pathMatch: 'full'},
-    {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'} // ленивая загрузка
+    // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'} // ленивая загрузка старый вариант
+    {path: 'about', loadChildren: ()=> import('./about-page/about-page.module').then(m=>m.AboutPageModule)} // новый вариант
   ])],
   exports: [RouterModule]
 })
